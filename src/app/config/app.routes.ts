4a542f19f8dc899f83f65b6f1ed84a790d130760
yousefsalama14 <yousef.salama2010@gmail.com>
2025-03-15 07:40:08 +0200

    fix: update app routers

--- src/app/config/app.routes.ts
+++ src/app/config/app.routes.ts
@@ -35,6 +35,30 @@ export const routes: Routes = [
     canActivate: [AuthGuard],
   },
   {
+    path: 'doctors',
+    loadComponent: () => import('../features/doctors/components/doctor-list/doctor-list.component')
+      .then(m => m.DoctorListComponent),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'doctors/new',
+    loadComponent: () => import('../features/doctors/components/doctor-form/doctor-form.component')
+      .then(m => m.DoctorFormComponent),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'prescriptions',
+    loadComponent: () => import('../features/prescriptions/components/prescription-list/prescription-list.component')
+      .then(m => m.PrescriptionListComponent),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'prescriptions/new',
+    loadComponent: () => import('../features/prescriptions/components/prescription-form/prescription-form.component')
+      .then(m => m.PrescriptionFormComponent),
+    canActivate: [AuthGuard],
+  },
+  {
     path: 'login',
     loadComponent: () =>
       import('../features/auth/login/login.component').then((m) => m.LoginComponent),
